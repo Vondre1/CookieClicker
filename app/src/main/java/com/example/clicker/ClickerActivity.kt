8250301c91b33a4fd2cb2ff1d5020070fbc3ffe7
Vondre <artemf1010@gmail.com>
2025-12-25 10:57:49 +0300

    импортировал binding и переделал ClickerActivity под него

--- app/src/main/java/com/example/clicker/ClickerActivity.kt
+++ app/src/main/java/com/example/clicker/ClickerActivity.kt
@@ -2,26 +2,24 @@ package com.example.clicker
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.Button
-import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.clicker.databinding.ActivityClickerBinding
 
 class ClickerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var binding: ActivityClickerBinding
+
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        setContentView(R.layout.activity_clicker)
 
-        val back = findViewById<ConstraintLayout>(R.id.clicker_background)
+        binding = ActivityClickerBinding.inflate(layoutInflater)
 
-        ViewCompat.setOnApplyWindowInsetsListener(back) { v, insets ->
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.clickerBackground) { v, insets ->
             val bars = insets.getInsets(
                 WindowInsetsCompat.Type.statusBars() or WindowInsetsCompat.Type.displayCutout()
             )
@@ -34,16 +32,10 @@ class ClickerActivity : AppCompatActivity() {
         val image_id = intent.getIntExtra("IMAGE_ID", R.drawable.just_cookie)
         val finalCookieName = intent.getStringExtra("finalCookieName")
 
-        back.setBackgroundResource(back_id)
-
-        val cookieImage: ImageView = findViewById(R.id.cookieImage)
-        cookieImage.setImageResource(image_id)
-
-        
-
-        val logout: ImageView = findViewById(R.id.logout)
+        binding.clickerBackground.setBackgroundResource(back_id)
+        binding.cookieImage.setImageResource(image_id)
         val auth = AuthManager(this)
-        logout.setOnClickListener {
+        binding.logout.setOnClickListener {
             auth.logout()
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
